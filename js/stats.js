a6ce268f386d0ad7fc6b128b57b9d44616dcd21e
Maksim Kokhtenko <makskokhwork@yandex.ru>
2020-01-24 14:36:14 +0300

    refactoring

--- js/stats.js
+++ js/stats.js
@@ -3,37 +3,40 @@
 window.renderStatistics = function (ctx, names, times) {
 
   // Параметры
-  var textFill = '#000000';
+  var TEXT_FILL = '#000000';
+  var CLOUD_POSITION = [100, 10, 420, 270];
+  var SHADOW_POSITION = [110, 20, 420, 270];
 
-  var barWidth = 40;
-  var initialX = 120;
-  var initialY = 245;
-  var indent = 90;
-  var indentName = 20;
-  var indentTime = 15;
+  var BAR_WIDTH = 40;
+  var INITIAL_X = 120;
+  var INITIAL_Y = 245;
+  var INDENT = 90;
+  var INDENT_NAME = 20;
+  var INDENT_TIME = 15;
 
-  var histogramHeight = 150;
-  var step = histogramHeight / (getMaxValue(times) - 0);
+  var HISTOGRAM_HEIGHT = 150;
+  var STEP = HISTOGRAM_HEIGHT / (getMaxValue(times) - 0);
 
   // Тень облака
   ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
   ctx.fillRect(110, 20, 420, 270);
+  ctx.fillRect.apply(ctx, SHADOW_POSITION);
 
   // Облако
   ctx.fillStyle = '#ffffff';
-  ctx.strokeRect(100, 10, 420, 270);
-  ctx.fillRect(100, 10, 420, 270);
+  ctx.strokeRect.apply(ctx, CLOUD_POSITION);
+  ctx.fillRect.apply(ctx, CLOUD_POSITION);
 
   // Текст
-  ctx.fillStyle = textFill;
+  ctx.fillStyle = TEXT_FILL;
   ctx.font = '16px PT Mono';
   ctx.fillText('Ура вы победили!', 120, 40);
   ctx.fillText('Список результатов: ', 120, 60);
 
 
   function getMaxValue(array) {
-    var max = -1;
-    for (var i = 0; i < array.length; i++) {
+    var max = array[0];
+    for (var i = 1; i < array.length; i++) {
       var value = array[i];
       if (value > max) {
         max = value;
@@ -52,15 +55,15 @@ window.renderStatistics = function (ctx, names, times) {
   }
 
   for (var i = 0; i < times.length; i++) {
-    var barHeight = times[i] * step;
-    var getY = initialY - times[i] * step;
-    var getX = initialX + indent * i;
+    var barHeight = times[i] * STEP;
+    var positionY = INITIAL_Y - times[i] * STEP;
+    var positionX = INITIAL_X + INDENT * i;
 
     ctx.fillStyle = fillBarColor(names[i]);
-    ctx.fillRect(getX, getY, barWidth, barHeight);
+    ctx.fillRect(positionX, positionY, BAR_WIDTH, barHeight);
 
-    ctx.fillStyle = textFill;
-    ctx.fillText(names[i], getX, initialY + indentName);
-    ctx.fillText(times[i].toFixed(0), getX, getY - indentTime);
+    ctx.fillStyle = TEXT_FILL;
+    ctx.fillText(names[i], positionX, INITIAL_Y + INDENT_NAME);
+    ctx.fillText(times[i].toFixed(0), positionX, positionY - INDENT_TIME);
   }
 };
